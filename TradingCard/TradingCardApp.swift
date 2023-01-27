6023a8259ede2917fd3fe2b3478aca1b2119e06c
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-01-26 20:18:23 -0500

    Initial Commit

---
+++ TradingCard/TradingCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardApp.swift
+//  TradingCard
+//
+//  Created by Emanuel Gross von Trockau on 2023-01-26.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
