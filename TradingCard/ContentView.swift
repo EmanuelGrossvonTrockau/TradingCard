43485d9f0c41ab8e129a5e1b46f00e1c565882c2
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-01-26 20:29:20 -0500

    restart

--- TradingCard/ContentView.swift
+++ TradingCard/ContentView.swift
@@ -9,13 +9,98 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            
+            LinearGradient(colors: [.white, .black, .white],
+                           startPoint: .leading,
+                           endPoint: .trailing)
+            .ignoresSafeArea()
+            .padding(.bottom, 160)
+            
+            
+            
+            Text("Thomas Müller")                .foregroundColor(.white)
+            Spacer()
+                .bold()
+            Color.white
+                .padding(7)
+                .frame(height: 420)
+            
+            HStack{
+                LinearGradient(colors: [.white, .black, .white],
+                               startPoint: .leading,
+                               endPoint: .trailing)
+                              
+                
+                .frame(height: 400)
+                .padding(10)
+                
+                
+            }
+            Color.white
+                .frame(width: 275, height: 270)
+            
+            
+            Image("Müller" )
+                .resizable()
+            
+                .frame(width: 265, height: 265)
+            
+            VStack{
+                Spacer()
+                    .frame(height: 300)
+                HStack{
+                    
+                    Image("DFB")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 120)
+                    
+                    Spacer()
+                }
+                
+            }
+            VStack{
+                
+                Spacer()
+                    .frame(height: 340)
+                HStack{
+                    
+                    Spacer()
+                    Image("German")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 120)
+                    
+                    
+                    
+                }
+                
+            }
+            VStack{
+                Spacer()
+                    .frame(height: 550)
+                Text("Thomas Müller")
+                    .font(Font.system(size: 25, weight: .bold))
+                    .foregroundColor(.red)
+                
+                HStack{
+                    
+                    Text("ZOM")
+                        .foregroundColor(.black)
+                        .font(Font.system(size: 40, weight: .bold))
+                    Spacer()
+                    Text("13")
+                        .font(Font.system(size: 40, weight: .bold))
+                        .foregroundColor(.yellow)
+                    
+                }
+            
+               
+                }
+            
+            
         }
-        .padding()
     }
 }
 
---
+++ TradingCard/Struc.swift
@@ -0,0 +1,8 @@
+//
+//  Struc.swift
+//  TradingCard
+//
+//  Created by Emanuel Gross von Trockau on 2023-01-26.
+//
+
+import Foundation
